943532c6953eed7e029a48ea31f78771508db88e
AugustanHalo <tannd1008@gmail.com>
2024-05-17 14:15:11 +0700

    Add project files.

---
+++ Source/SE183491ConsoleEFApp/SE183491ConsoleEFApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using SE183491ConsoleEFApp.Models;
+
+Console.WriteLine("Hello, World!");
+
+
+var _context = new NET1814_212_4_JewelryStoreContext(new Microsoft.EntityFrameworkCore.DbContextOptions<NET1814_212_4_JewelryStoreContext>());
+var customerAccounts = _context.SiCustomers.ToArray();
+
+foreach (var customer in customerAccounts)
+{
+    Console.WriteLine(customer.CustomerId  + customer.Name);
+}
